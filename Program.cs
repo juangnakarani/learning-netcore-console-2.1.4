870e8c35e05e919c7346f1bd515dd76531aac7e3
juang nakarani <juang.nakarani@gmail.com>
2018-02-14 22:00:47 +0700

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learning_netcore_console_2._1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
